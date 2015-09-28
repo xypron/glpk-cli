4aa5a6d0017b16bdecc8374d7dca173aa46663e1
Heinrich Schuchardt <xypron.glpk@gmx.de>
2015-09-28 20:18:13 +0200

    relax4.cs: reformat example code
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- examples/csharp/relax4.cs
+++ examples/csharp/relax4.cs
@@ -31,38 +31,42 @@ public class Relax4
 				GLPK.GLP_CLI_A_SIZE);
 			do {
 				GLPK.glp_cli_set_numeric_locale ("C");
-				ret = GLPK.glp_read_mincost (g,
-				                             GLPK.GLP_CLI_V_RHS,
-				                             GLPK.GLP_CLI_A_LOW,
-				                             GLPK.GLP_CLI_A_CAP,
-				                             GLPK.GLP_CLI_A_COST,
-				                             filename);
+				ret = GLPK.glp_read_mincost (
+					g,
+					GLPK.GLP_CLI_V_RHS,
+					GLPK.GLP_CLI_A_LOW,
+					GLPK.GLP_CLI_A_CAP,
+					GLPK.GLP_CLI_A_COST,
+					filename);
 				if (ret != 0) {
 					break;
 				}
-				ret = GLPK.glp_mincost_relax4 (g,
-				                               GLPK.GLP_CLI_V_RHS,
-				                               GLPK.GLP_CLI_A_LOW,
-				                               GLPK.GLP_CLI_A_CAP,
-				                               GLPK.GLP_CLI_A_COST,
-				                               0,
-				                               sol,
-				                               GLPK.GLP_CLI_A_X,
-				                               GLPK.GLP_CLI_A_RC);
-				Console.WriteLine ("ret = {0}; sol = {1}", ret,
-				                   GLPK.doubleArray_getitem (sol, 0));
+				ret = GLPK.glp_mincost_relax4 (
+					g,
+					GLPK.GLP_CLI_V_RHS,
+					GLPK.GLP_CLI_A_LOW,
+					GLPK.GLP_CLI_A_CAP,
+					GLPK.GLP_CLI_A_COST,
+					0,
+					sol,
+					GLPK.GLP_CLI_A_X,
+					GLPK.GLP_CLI_A_RC);
+				Console.WriteLine (
+					"ret = {0}; sol = {1}", ret,
+					GLPK.doubleArray_getitem (sol, 0));
 				if (ret != 0) {
 					break;
 				}
 				for (int i = 1; i < g.nv; i++) {
 					glp_vertex v = GLPK.glp_cli_vertex_get (g, i);
 					for (glp_arc a = v.out_; a != null;
-		           a = a.t_next) {
+					     a = a.t_next) {
 						glp_vertex w = a.head;
-						Console.WriteLine ("arc {0}->{1}: x = {2}; rc = {3}",
-						                   v.i, w.i,
-						                   GLPK.glp_cli_arc_get_data (a).x,
-						                   GLPK.glp_cli_arc_get_data (a).rc);
+						Console.WriteLine (
+							"arc {0}->{1}: x = {2}; rc = {3}",
+							v.i, w.i,
+							GLPK.glp_cli_arc_get_data (a).x,
+							GLPK.glp_cli_arc_get_data (a).rc);
 					}
 				}
 				// signal success
